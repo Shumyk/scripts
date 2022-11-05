c4a0acd8785bb4d425eaca3b7218b5a27c7ca8b7
Shumyk <dima.shumyk@gmail.com>
2022-11-05 22:28:48 +0200

    added parsing of args

--- kdeploy/cmd/root.go
+++ kdeploy/cmd/root.go
@@ -2,19 +2,33 @@ package cmd
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
-var kdeploy = cobra.Command{
-	Use:   "kdeploy",
-	Short: "k[8s]deploy - deploy from the terminal",
-	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("kdeploy yay")
-	},
-}
+var (
+	microservice  string
+	imageListSize int = 20
+
+	kdeploy = cobra.Command{
+		Use:   "kdeploy microservice [image list size]",
+		Short: "k[8s]deploy - deploy from the terminal",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Fprintln(os.Stderr, "usage")
+				os.Exit(1)
+			}
+			microservice = args[0]
+			if len(args) == 2 {
+				imageListSize, _ = strconv.Atoi(args[1])
+			}
+
+			fmt.Fprintln(os.Stdout, "kdeploy", microservice, imageListSize)
+		},
+	}
+)
 
 func Execute() {
 	if err := kdeploy.Execute(); err != nil {
