ffd511fdd1805b6eca71854e29585c69a90d0cc0
Shumyk <dima.shumyk@gmail.com>
2022-11-13 16:40:41 +0200

    removed unused args

--- kdeploy/cmd/root.go
+++ kdeploy/cmd/root.go
@@ -8,29 +8,20 @@ import (
 )
 
 var (
-	microservice  string
-	imageListSize int
+	microservice string
 
 	kdeploy = cobra.Command{
 		Use:   "kdeploy microservice",
 		Short: "k[8s]deploy - deploy from the terminal",
 		Run:   run,
+		Args:  cobra.ExactArgs(1),
 	}
 )
 
 func run(cmd *cobra.Command, args []string) {
-	validateArgs(args)
 	microservice = args[0]
 
-	fmt.Fprintln(os.Stdout, "kdeploy", microservice, imageListSize)
 	KDeploy()
-}
-
-func validateArgs(args []string) {
-	if len(args) < 1 {
-		fmt.Fprintln(os.Stderr, "usage")
-		os.Exit(1)
-	}
 }
 
 func Execute() {
@@ -38,8 +29,4 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	kdeploy.PersistentFlags().IntVarP(&imageListSize, "list", "l", 20, "amount of images to fetch")
 }
