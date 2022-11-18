ab794c84023c8165aac3d5b9aa296f411de72572
Shumyk <dima.shumyk@gmail.com>
2022-11-18 22:09:51 +0200

    generified generics !!

--- kdeploy/cmd/util/structures.go
+++ kdeploy/cmd/util/structures.go
@@ -1,18 +1,22 @@
 package cmd
 
-func SliceMapping[I any, O any](inputs []I, mapping func(I) O) (output []O) {
+type SliceMap[I, O any] func(I) O
+
+func SliceMapping[I, O any](inputs []I, from SliceMap[I, O]) (output []O) {
 	output = make([]O, len(inputs))
 	for i, input := range inputs {
-		output[i] = mapping(input)
+		output[i] = from(input)
 	}
 	return
 }
 
-func MapToSliceMapping[I any, O any](inputs map[string]I, mapping func(string, I) O) (output []O) {
+type MapToSlice[K, V, O any] func(K, V) O
+
+func MapToSliceMapping[K comparable, V, O any](inputs map[K]V, from MapToSlice[K, V, O]) (output []O) {
 	output = make([]O, len(inputs))
 	position := 0
 	for key, value := range inputs {
-		output[position] = mapping(key, value)
+		output[position] = from(key, value)
 		position++
 	}
 	return
