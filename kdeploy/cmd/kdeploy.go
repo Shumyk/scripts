519fa755a807ddf7524c924d90b332b92b41ff5c
Shumyk <dima.shumyk@gmail.com>
2022-11-22 20:05:46 +0200

    merged kdeploy.previous.go into kdeploy.go

--- kdeploy/cmd/kdeploy.go
+++ kdeploy/cmd/kdeploy.go
@@ -1,13 +1,30 @@
 package cmd
 
-import prompt "shumyk/kdeploy/cmd/prompt"
+import (
+	prompt "shumyk/kdeploy/cmd/prompt"
+	. "shumyk/kdeploy/cmd/util"
+)
 
 func KDeploy() {
 	DeployNew()
+}
+
+func KDeployPrevious() {
+	previous := GetPrevious()[microservice]
+	TerminateOnEmpty(previous, "previous deployments of", microservice, "absent")
+	DeployPrevious(previous)
 }
 
 func KDeployWithRegistry() {
 	repos := ListRepos()
 	microservice = prompt.RepoSelect(repos)
 	DeployNew()
+}
+
+func KDeployPreviousWithRegistry() {
+	repos := GetPrevious().Keys()
+	TerminateOnEmpty(repos, "previous deployments absent")
+
+	microservice = prompt.RepoSelect(repos)
+	KDeployPrevious()
 }
