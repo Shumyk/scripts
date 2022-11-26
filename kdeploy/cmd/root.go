ef888f53f1a80600ba4fd245cf96480b932b74cb
Shumyk <dima.shumyk@gmail.com>
2022-11-26 19:38:33 +0200

    added descriptions for commands

--- kdeploy/cmd/root.go
+++ kdeploy/cmd/root.go
@@ -8,13 +8,57 @@ import (
 var (
 	previousMode bool
 
-	// TODO: more info here needed
 	kdeploy = cobra.Command{
-		Use:    "kdeploy [microservice]",
-		Short:  "k[8s]deploy - deploy from the terminal",
-		Run:    kdeployRun,
+		Use:   "kdeploy [microservice]",
+		Short: "k[8s]deploy - deploy from the terminal",
+		Long: `Searches for images of requested microservice in Google Container Registry,
+Prompts you to interactively select an image for deployment (arrows navigation, search features),
+And sets the selected image in the workload.
+If microservice was not specified - it obtains possible repositories from the registry and prompts you to select it first.
+
+kdeploy requires two configuration properties - registry and repository.
+The registry is where to look for your images (e.x. us.gcr.io), and the repository is the path to your images.
+Set them using:
+    kdeploy config set [registry|repository] [value]
+Or  kdeploy config edit
+
+Assumed that all workloads are of Deployment type. If some are StatefulSets, set them in configurations:
+    kdeploy config set statefulsets ms-events,ms-core
+
+kdeploy remembers every deployment you made and allows you to redeploy previous images.
+    kdeploy --previous [microservice]`,
 		Args:   cobra.MaximumNArgs(1),
 		PreRun: InitConfig,
+		Run:    kdeployRun,
+	}
+
+	// configurations commands
+	configCmd = cobra.Command{
+		Use:              "config [action] [args]...",
+		Short:            "View, edit, set configurations",
+		PersistentPreRun: LoadConfiguration,
+	}
+	configViewCmd = cobra.Command{
+		Use:   "view",
+		Short: "Displays current configuration.",
+		Run:   runConfigView,
+		Args:  cobra.NoArgs,
+	}
+	configEditCmd = cobra.Command{
+		Use:   "edit",
+		Short: "Basically, opens vim editor on configuration file.",
+		Run:   RunConfigEdit,
+		Args:  cobra.NoArgs,
+	}
+	configSetCmd = cobra.Command{
+		Use:   "set [property] [value]",
+		Short: "Conveniently set properties.",
+		Long: `Conveniently set properties.
+Use ',' delimiter (without space) for array type properties (e.x. statefulsets).`,
+		Example: `  kdeploy config set registry us.gcr.io  
+  kdeploy config set statefulsets ms-events,ms-core`,
+		Run:  RunConfigSet,
+		Args: cobra.ExactArgs(2),
 	}
 )
 
@@ -44,33 +88,12 @@ func deployMicroservice(args []string) {
 }
 
 func Execute() {
-	err := kdeploy.Execute()
-	ErrorCheck(err, "Failed to execute kdeploy :|")
+	ErrorCheck(kdeploy.Execute(), "Failed to execute kdeploy :|")
 }
 
 func init() {
-	kdeploy.Flags().BoolVarP(&previousMode, "previous", "p", false, "deploy previous")
+	kdeploy.Flags().BoolVarP(&previousMode, "previous", "p", false, "deploy previous images")
 
-	configCmd := cobra.Command{
-		Use:              "config [action] [args]...",
-		PersistentPreRun: LoadConfiguration,
-	}
-	configViewCmd := cobra.Command{
-		Use:  "view",
-		Run:  runConfigView,
-		Args: cobra.NoArgs,
-	}
-	configEditCmd := cobra.Command{
-		Use:  "edit",
-		Run:  RunConfigEdit,
-		Args: cobra.NoArgs,
-	}
-	configSetCmd := cobra.Command{
-		Use: "set [property] [value]",
-		// TODO: docs about setting array property types
-		Run:  RunConfigSet,
-		Args: cobra.ExactArgs(2),
-	}
-	kdeploy.AddCommand(&configCmd)
 	configCmd.AddCommand(&configViewCmd, &configSetCmd, &configEditCmd)
+	kdeploy.AddCommand(&configCmd)
 }
